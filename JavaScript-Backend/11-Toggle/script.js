34107a48660d25cef84935f69494848f13ac460a
Erismar <125566354+Erismar17@users.noreply.github.com>
2023-11-24 17:47:37 -0500

    Delete JavaScript-Backend directory

--- JavaScript-Backend/11-Toggle/script.js
+++
@@ -1,7 +0,0 @@
-const body = document.querySelector("body");
-const toggle = document.getElementById("toggle");
-
-toggle.addEventListener('click', () => {
-    toggle.classList.toggle("toggleBlanco");
-    body.classList.toggle("toggleBlanco");
-})
